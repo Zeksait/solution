66a293ef2ef49c1e1a0c0d84b262401601f69c58
AndreyBorodin <kakoitov@gmail.com>
2024-03-20 07:00:39 +0300

    поправил инструкцию, мелкие доработки

--- README.md
+++ README.md
@@ -1,14 +1,36 @@
 # solution
 Реализация тестового задания для Элвис
 
-1) перейти в папку: cd solution
+1) перейти в папку: 
+    ```sh
+    cd solution
+    ```
 1) в файле env заменить параметры своими: nano env
+    ```sh
+    nano env
+    ```
 2) переименовать env в .env: mv env .env
+    ```sh
+    mv env .env
+    ```
 3) создать папку postgres-db: mkdir postgres-db
+    ```sh
+    mkdir postgres-db
+    ```
 4) сбилдить контейнеры и запустить: docker compose build && docker compose up -d
+    ```sh
+    docker compose build && docker compose up -d
+    ```
 5) Создать таблицу: docker compose exec flask python manage.py create_db
+    ```sh
+    docker compose exec flask python manage.py create_db
+    ```
 6) Заполнить таблицу данными: docker compose exec flask python manage.py create_data
+    ```sh
+    docker compose exec flask python manage.py create_data
+    ```
 7) проверить в браузере: http://localhost:5000/
+
 
 Зайти в шелл: docker compose exec flask python manage.py shell
 Посмотреть логи фласк в реальном времени: docker compose logs -f flask
--- web_app/route.py
+++ web_app/route.py
@@ -1,17 +1,38 @@
 from datetime import datetime
 
 from flask import Blueprint, render_template, request, redirect, url_for
+from sqlalchemy import func
+
 from models import *
 
 app_route = Blueprint('route', __name__, template_folder='templates')
 
+
 @app_route.route('/')
 def index():
    """Статистические данные системы"""
-   return render_template('index.html')
+   users_count = User.query.count()
+   achievements_count = Achievement.query.count()
+   return render_template(
+      'index.html',
+      users_count = users_count,
+      achievements_count=achievements_count,
+      user_achievements_count = User.query.with_entities(
+         UserAchievement.achievement_id,
+         func.count(UserAchievement.achievement_id)
+         )
+         .group_by(UserAchievement.achievement_id)
+         .all(),
+      )
 
 
-@app_route.route('/user/<user_id>')
+@app_route.route('/users')
+def get_users():
+   """предоставляет информацию о всех доступных пользователях"""
+   return render_template('users.html', users=User.query.all())
+
+
+@app_route.route('/users/<user_id>')
 def get_user(user_id):
    """ предоставляет информацию о пользователе"""
    user = User.query.filter_by(id=user_id).first()
@@ -20,28 +41,12 @@ def get_user(user_id):
       return render_template('user.html', user=user, achievements=achievements)
    else:
       return render_template('404.html')
-   
-
-@app_route.route('/users')
-def get_users():
-   """предоставляет информацию о всех доступных пользователях"""
-   return render_template('users.html', users=User.query.all())
 
 
 @app_route.route('/achievements')
 def get_achievements():
    """предоставляет информацию о всех доступных достижениях"""
    return render_template('achievements.html', achievements=Achievement.query.all())
-
-
-@app_route.route('/achievements/<achievement_id>')
-def get_achievement(achievement_id):
-   """ предоставляет информацию о достижении"""
-   achievement = Achievement.query.filter_by(id=achievement_id).first()
-   if achievement:
-      return render_template('achievement.html', achievement=achievement)
-   else:
-      return render_template('404.html')
 
 
 @app_route.route('/achievements/add', methods=['GET', 'POST'])
@@ -59,10 +64,21 @@ def add_achievement():
       return redirect(url_for('route.get_achievements'))
    else:
       return render_template('add_achievement.html')
-   
+
+
+@app_route.route('/achievements/<achievement_id>')
+def get_achievement(achievement_id):
+   """ предоставляет информацию о достижении"""
+   achievement = Achievement.query.filter_by(id=achievement_id).first()
+   if achievement:
+      return render_template('achievement.html', achievement=achievement)
+   else:
+      return render_template('404.html')
+
 
 @app_route.route('/achievements/award', methods=['POST'])
 def award_user():
+   """Наградить пользователя"""
    try:
       user_id = int(request.form.get('user_id'))
       achievement_id = int(request.form.get('achievement_id'))
@@ -71,10 +87,3 @@ def award_user():
       return redirect(url_for('route.get_user', user_id=user_id))
    except:
       return "Error"
-
-
-@app_route.route('/')
-def set_achievemen_to_user():
-   """Выдать достижения пользователю с сохранением времени выдачи 
-   (сохранять связь пользователя с достижением и датой выдачи)"""
-   return str(User.query.all())
